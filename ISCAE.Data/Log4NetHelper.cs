3a59911c16f0fdbc7f60759572d0695d2c2f649f
bbahieda <bbahieda@gmail.com>
2017-04-05 16:42:14 +0000

    Add Log4Net and his configuration

---
+++ ISCAE.Data/Log4NetHelper.cs
@@ -0,0 +1,20 @@
+﻿using log4net;
+using log4net.Config;
+using System;
+
+namespace ISCAE.Data
+{
+    public class Log4NetHelper
+    {
+        private static ILog _logger;
+
+        public static ILog GetLogger(Type type)
+        {
+            if (_logger != null)
+                return _logger;
+            XmlConfigurator.Configure();
+            _logger = LogManager.GetLogger(type);
+            return _logger;
+        }
+    }
+}
