305bc2af4ba542ef927536b12ab81ee0951de954
bbahieda <bbahieda@gmail.com>
2017-04-06 12:20:35 +0000

    Rename Logic to Services and add a new interfaces

---
+++ ISCAE.Business/Services/IAdministrateurService.cs
@@ -0,0 +1,19 @@
+﻿using ISCAE.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISCAE.Business.Services
+{
+    public interface IAdministrateurService : ICommonService<Administrateur>
+    {
+        Administrateur GetUserByAuth(string login, string password);
+        Administrateur GetUserByLogin(string login);
+        Administrateur GetUserByEmail(string email);
+        Administrateur GetUserByTelephone(string telephone);
+        IEnumerable<Administrateur> GetActiveUsers();
+        IEnumerable<Administrateur> GetNonActiveUsers();
+    }
+}
---
+++ ISCAE.Business/Services/ICommonService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISCAE.Business.Services
+{
+    public interface ICommonService<TEntity> where TEntity : class
+    {
+        TEntity Get(int id);
+        IEnumerable<TEntity> GetAll();
+        IEnumerable<TEntity> Find(Expression<Func<TEntity, bool>> predicate);
+        void Add(TEntity entity);
+        void Edit(TEntity entity);
+        void Delete(TEntity entity);
+        void Save();
+    }
+}
