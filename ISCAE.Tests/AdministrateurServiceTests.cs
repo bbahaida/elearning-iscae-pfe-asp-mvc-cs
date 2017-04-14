b050b584f288f60b7384c4ffba6922e378dca983
bbahieda <bbahieda@gmail.com>
2017-04-14 22:35:38 +0000

    Initialize web project setup  homeLayout

---
+++ ISCAE.Tests/AdministrateurServiceTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using ISCAE.Data.Repositories;
+using System.Collections;
+using ISCAE.Data;
+using System.Collections.Generic;
+
+namespace ISCAE.Tests
+{
+    [TestClass]
+    public class AdministrateurServiceTests
+    {
+        
+        
+
+        [TestMethod]
+        public void Should_GetUserByAuth_Return_Single()
+        {
+            // eywe la5bar chnhi
+        }
+    }
+}
